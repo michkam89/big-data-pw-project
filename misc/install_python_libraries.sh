517ea9ac075418bb67201d783ecfa54228e36a6f
Michal Kaminski <michkam89@gmail.com>
2023-01-24 19:36:09 +0100

    another batch commit; results done

--- misc/install_python_libraries.sh
+++ misc/install_python_libraries.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo python3 -m pip install matplotlib plotnine wordcloud bs4 html nltk pandas
+sudo python3 -m pip install matplotlib plotnine wordcloud bs4 html nltk pandas patchworklib boto3 pyspark jupyter-client scikit-misc
